787df1a51ff145e99f99d2c6af113a9b8bcbdfe8
Woody <sarawut.core@gmail.com>
2025-02-23 16:45:11 +0700

    refactor: add comment description.

--- src/Components/SuperCoin.jsx
+++ src/Components/SuperCoin.jsx
@@ -2,13 +2,20 @@ import React, { useState, useEffect } from "react";
 import { useSelector } from "react-redux";
 
 const SuperCoins = () => {
+  /// This lets functional components set up and care for their own state variables,
+  /// allowing them to store data that can change over time.
   const [superCoins, setSuperCoins] = useState(0);
+
+  /// Now you need to retrieve the cartItems from the cart slice of the Redux store’s state to
+  /// get the total quantity of number of products using the useSelector hook.
   const cartItems = useSelector((state) => state.cart.cartItems);
   const totalAmount = cartItems.reduce(
     (total, item) => total + item.price * item.quantity,
     0
   );
 
+  /// This lets functional components do side effects like getting data
+  /// or changing the DOM after every view, which is how lifecycles work.
   useEffect(() => {
     if (totalAmount >= 100 && totalAmount < 200) {
       setSuperCoins(10);
